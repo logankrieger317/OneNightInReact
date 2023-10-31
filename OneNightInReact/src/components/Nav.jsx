1bed321715ccdb6fb857a8853170ffc3d3ccdef8
Joshua Thomas <jthoma10@hotmail.com>
2023-10-31 13:24:53 -0600

    setting up

--- OneNightInReact/src/components/Nav.jsx
+++ OneNightInReact/src/components/Nav.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 export default function Nav() {
     return(
         <div className="Nav">
-            
+            <div>
+                <Link to="/"> Home </Link>
+            </div>
         </div>
     )
 }
